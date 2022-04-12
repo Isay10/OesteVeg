d94136d7f8c748ffb5b62a175fd31975ecab9b8a
Patricio Giménez <patricio.n.gimenez@gmail.com>
2022-04-11 23:00:41 -0300

    on add + cart

--- oesteveg-gimenez/src/components/ItemDetail/ItemDetail.js
+++ oesteveg-gimenez/src/components/ItemDetail/ItemDetail.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { onAdd } from "../../Common/commonMethods.js";
+import { useNavigate } from "react-router-dom";
 import { productList } from "../../services/mock.service.products.js";
 import ItemCount from "../ItemCount/ItemCount";
+import { Button } from "@mui/material";
 import { useParams } from "react-router-dom";
 import "./ItemDetail.css";
 
@@ -9,6 +10,18 @@ const ItemDetail = () => {
   const { id } = useParams();
   const [detail, setDetail] = useState([]);
   const [product, setProduct] = useState({});
+  const [gotProduct, setGotProduct] = useState(false);
+
+  let navigate = useNavigate();
+
+  const onAdd = (qty) => {
+    alert(
+      `Se ${qty > 1 ? "han" : "ha"} agregado ${qty} ${
+        qty > 1 ? "productos" : "producto"
+      } al carrito`
+    );
+    setGotProduct(true);
+  };
 
   const getItem = new Promise((res, rej) => {
     setTimeout(() => {
@@ -34,11 +47,13 @@ const ItemDetail = () => {
 
   const filterProductById = (array, id) => {
     return array.map((product) => {
-      if (product.id == id) {
+      if (product.id === id) {
         return setProduct(product);
       }
     });
   };
+
+  console.log("GotProduct: " + JSON.stringify(gotProduct));
 
   return (
     <div className="detail-list-container">
@@ -54,12 +69,27 @@ const ItemDetail = () => {
         <h1>{detail[id]?.name}</h1>
         <h2>${detail[id]?.price}</h2>
         <h3>{detail[id]?.stock} unidades</h3>
-        <ItemCount
-          id={id}
-          stock={detail[id]?.stock}
-          onAdd={onAdd}
-          initial={detail[id]?.stock === 0 ? 0 : 1}
-        />
+        {!gotProduct ? (
+          <ItemCount
+            id={id}
+            stock={detail[id]?.stock}
+            onAdd={onAdd}
+            initial={detail[id]?.stock === 0 ? 0 : 1}
+          />
+        ) : (
+          <Button
+            variant="contained"
+            style={{
+              backgroundColor: "#a6ffa5",
+              borderRadius: "10px",
+              height: "50%",
+              marginTop: "10px",
+            }}
+            onClick={() => navigate(`/cart`)}
+          >
+            Ir al carrito
+          </Button>
+        )}
       </div>
     </div>
   );
